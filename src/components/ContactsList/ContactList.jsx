f9293f2af813a7992f7ac035d4919632caa9e951
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-08-19 12:05:08 -0700

    fixed

--- src/components/ContactsList/ContactList.jsx
+++ src/components/ContactsList/ContactList.jsx
@@ -1,15 +1,19 @@
 import { nanoid } from '@reduxjs/toolkit';
 import css from './ContactList.module.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { getContacts, getFilter } from 'Redux/selectors';
+import { getContacts, getError, getFilter, getIsLoading } from 'Redux/selectors';
 import { useEffect } from 'react';
 import { deleteContactsThunk, getContactsThunk } from 'Redux/thunk';
+import Loader from 'components/Loader';
 
 
 
 function ContactList() {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  error&&console.log(error);
   
   const dispatch = useDispatch();
 
@@ -29,11 +33,11 @@ function ContactList() {
         contact.name.toLowerCase().includes(normalizedFilter)
       );
     };
-  
     const visibleContacts = getVisibleContacts();
 
   return (
     <ul className={css.list}>
+      {isLoading && <div className={css.Loader}><Loader /></div>}
       {visibleContacts.map(({ id = nanoid(), name, phone }) => (
         <li key={id} className={css.item}>
           <p className={css.name}>{name}</p>
